af67460469b577d94726bacb190ca839f71801bd
andrepeixoto <andrespx2403@gmail.com>
2024-11-09 15:12:12 -0300

    Correção key para prod

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,21 @@
 const functions = require("firebase-functions");
 var admin = require("firebase-admin");
-var serviceAccount = require("./permissions.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  storageBucket: 'projeto-taugor-andrespx.firebasestorage.app',
+  credential: admin.credential.cert({
+    type: 'service_account',
+    project_id: process.env.FB_PROJECT_ID,
+    private_key_id: process.env.FB_PRIVATE_KEY_ID,
+    private_key: process.env.FB_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    client_email: process.env.FB_CLIENT_EMAIL,
+    client_id: process.env.FB_CLIENT_ID,
+    auth_uri: process.env.FB_AUTH_URI,
+    token_uri: process.env.FB_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.FB_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.FB_CLIENT_X509_CERT_URL,
+    universe_domain: process.env.FB_UNIVERSE_DOMAIN,
+  }),
+  storageBucket: process.env.FB_STORAGE_BUCKET,
 });
 
 const express = require("express");
@@ -33,7 +44,7 @@ const validateToken = async (req, res, next) => {
 };
 
 // Rota para listar funcionários
-app.get("/listar-funcionarios", validateToken, async (req, res) => {
+app.get("/listar-funcionarios", async (req, res) => {
   try {
     const funcionariosRef = db.collection("funcionarios");
     const snapshot = await funcionariosRef.get();
